e390e384bef6fa482d1d0ad1af675afa293d16fc
sasheppard <134110984+sasheppard@users.noreply.github.com>
2023-07-20 10:42:24 -0600

    Update streamlit_app.py
    add new item and special item

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega-3 and Blueberry Oatmeal')
 streamlit.text('Kale and Spinach Smoothie')
 streamlit.text('Hard-boiled Cage-free egg')
+streamlit.text('Avocado Toast')
+
+streamlit.header('Build Your Own Fruit Smoothie')
+
