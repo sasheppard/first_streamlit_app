999063ee8cacf42234aa8e7f345224ee0724a85c
sasheppard <134110984+sasheppard@users.noreply.github.com>
2023-07-20 20:08:51 -0600

    Update streamlit_app.py
    change to fetchall

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,9 +47,11 @@ my_cur = my_cnx.cursor()
 # fruit_load_list is a table in PC_RIVERY_DB in Snowflake
 my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
 # fetchone should return banana
-my_data_row = my_cur.fetchone()
+# my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 # streamlit.text("Hello from Snowflake:")
-streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_row)
+# streamlit.text("The fruit load list contains:")
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
 
                                      
