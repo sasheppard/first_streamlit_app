a97480be2eb09f15bed66c91e33668b899308731
sasheppard <134110984+sasheppard@users.noreply.github.com>
2023-07-20 21:30:32 -0600

    Update streamlit_app.py
    add func and button to insert fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,12 +84,23 @@ if streamlit.button("Get Fruit Load List"):
   streamlit.dataframe(my_data_rows)
                     
 
-streamlit.stop()
+# streamlit.stop()
 
 # ask to choose another fruit
-add_my_fruit = streamlit.text_input('What fruit do you want to add?', 'Apple')
-streamlit.write('Thanks for adding', add_my_fruit) 
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES ('from streamlit')")
+    return "Thanks for adding " + new_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a fruit to the list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_func = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_func)
+
+# add_my_fruit = streamlit.text_input('What fruit do you want to add?', 'Apple')
+# streamlit.write('Thanks for adding', add_my_fruit) 
 
 # adding the next line to demostrate control flow. It gets fired each time a text box changes
-my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES ('from streamlit')")
+# my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES ('from streamlit')")
 
