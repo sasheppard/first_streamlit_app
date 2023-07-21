e25d65ea771a078056b6052a4ffa1a9beacae2ac
sasheppard <134110984+sasheppard@users.noreply.github.com>
2023-07-20 20:00:37 -0600

    Update streamlit_app.py
    add db and schema to select stmt

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 # fruit_load_list is a table in PC_RIVERY_DB in Snowflake
-my_cur.execute("SELECT * FROM fruit_load_list")
+my_cur.execute("SELECT * FROM pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 # streamlit.text("Hello from Snowflake:")
 streamlit.text("The fruit load list contains:")
